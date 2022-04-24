3a8399ea6ef33ecf9de307a0c3bea747e8069b7e
anqiwu <anqiwu85@outlook.com>
2022-04-24 22:35:03 +0800

    22/4/24 Java Fun

--- hello_world.java
+++ hello_world.java
@@ -9,6 +9,8 @@ public class HelloWorld {
  * 1. All code in Java must be part of a class.
  * 2. Classes ara defined by public class CLASSNAME.
  * 3. We delimit the beginning and end of segments of code using { and }.
+ * Curly braces are very important in Java!
+ * Unlike Python, statements are grouped by braces, and NOT by indentation.
  * 4. All statements in Java must end in a semi-colon.
  * 5. For code to run, we need public static void main(String[] args){}.
  */
