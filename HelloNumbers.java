b7b75c612a9f58a5e91aa37ebf009ed52296714f
anqiwu <anqiwu85@outlook.com>
2022-04-29 21:44:11 +0800

    22/4/29 Java Fun

--- HelloNumbers.java
+++ HelloNumbers.java
@@ -3,9 +3,16 @@ public class HelloNumbers {
         int i = 0;
         int sum = 0;
         while (i < 10) {
-            System.out.println(sum);
+            /** In this case,System.out.print(sum + " ");,
+             * Java, strongly typed as it is,
+             * expects a string type result,
+             * and smoothly interprets the arguments
+             * as a string concatentation. */
+            System.out.print(sum + " ");
             i++;
             sum += i;
         }
+        //test string concatenation
+        System.out.println(5 + "10"); // print "510"
     }
 }
