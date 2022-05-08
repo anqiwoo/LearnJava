30d085c263c6f8bc790627535928589e8c57a8ef
anqiwu <anqiwu85@outlook.com>
2022-05-08 15:22:10 +0800

    22/5/8 Java Fun

---
+++ Chap2/TapeDeck.java
@@ -0,0 +1,11 @@
+class TapeDeck {
+    boolean canRecord = true;
+
+    void playTape() {
+        System.out.println("tape playing");
+    }
+
+    void recordTape() {
+        System.out.println("tape recording...");
+    }
+}
