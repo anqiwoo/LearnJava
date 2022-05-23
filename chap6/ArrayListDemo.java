3086f46474d056fdee78527a497bddf69d3d68d3
anqiwu <anqiwu85@outlook.com>
2022-05-23 15:31:05 +0800

    22/5/23 Java Fun

---
+++ chap6/ArrayListDemo.java
@@ -0,0 +1,19 @@
+class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> myList = new ArrayList<String>();
+        String s = "Hello";
+        myList.add(s);
+        String b = "World";
+        myList.add(b);
+        int theSize = myList.size();
+        System.out.println(theSize);
+        boolean isIn = myList.contains(s);
+        System.out.println(isIn);
+        int idx = myList.indexOf(b);
+        System.out.println(idx);
+        boolean empty = myList.isEmpty();
+        System.out.println(empty);
+        myList.remove(s);
+        System.out.println(myList);
+    }
+}
